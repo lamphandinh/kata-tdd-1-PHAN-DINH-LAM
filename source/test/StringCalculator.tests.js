ce2ae3f115464310b1118654fe80b9330f804668
PhanDinhLam <deansuoithongb@gmail.com>
2015-08-18 20:38:43 +0700

    step 1 , case 1 :  when string empty  - write test

--- source/test/StringCalculator.tests.js
+++ source/test/StringCalculator.tests.js
@@ -1,1 +1,6 @@
 ﻿// test
+describe('StringCalculator object', function () {
+	it('when emty string is used then return value is 0', function () {
+		expect(StringCalculator.Add('')).toEqual(0);
+    });
+});
