67746c897b7a59a50dc2abb310329c6fa44ea686
PhanDinhLam <deansuoithongb@gmail.com>
2015-08-18 20:45:40 +0700

    step 1 , case 2 : input string is 1 number - write test

--- source/test/StringCalculator.tests.js
+++ source/test/StringCalculator.tests.js
@@ -3,4 +3,7 @@ describe('StringCalculator object', function () {
 	it('when emty string is used then return value is 0', function () {
 		expect(StringCalculator.Add('')).toEqual(0);
     });
+	it('when one number is used then return value is that same number', function () {
+         expect(StringCalculator.Add('1')).toEqual(1);
+    });
 });
