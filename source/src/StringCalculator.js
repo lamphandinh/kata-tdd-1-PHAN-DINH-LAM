56432f9dd3a92d34a8418bbf211ff8599ff0fbdc
PhanDinhLam <deansuoithongb@gmail.com>
2015-08-18 21:51:09 +0700

    step 3 : handle new lines between numbers - refactor code

--- source/src/StringCalculator.js
+++ source/src/StringCalculator.js
@@ -2,14 +2,16 @@
 var StringCalculator = {
 	Add: function (number_string){
 		var total = 0,
-		numberArray = [];
+		numberArray = [],
+		step3Excep = ',\n',
+		delimiter = /,|\n/;
 		if(!number_string){
 			return total;
 		}
-		if(number_string.indexOf(',\n') > 0){
+		if(number_string.indexOf(step3Excep) > 0){
 			throw 'input is NOT ok';
 		}
-		numberArray = number_string.split(/,|\n/);
+		numberArray = number_string.split(delimiter);
 		for(var i = 0;i < numberArray.length;i++){
 			if(numberArray[i]) {
 				total += parseInt(numberArray[i]);
