a13e486325246d9096d9afb4f3d7ab2ee5b67df7
PhanDinhLam <deansuoithongb@gmail.com>
2015-08-21 19:57:43 +0700

    step 7,8,9  - implement code

--- source/src/StringCalculator.js
+++ source/src/StringCalculator.js
@@ -4,6 +4,8 @@ var StringCalculator = {
 		var total = 0,
 		step3Excep = ',\n',
 		delimiter = /,|\n/,
+		delimiter_regex = delimiter,
+		multi_delimiter = [],		
 		start_string_contain_spec_delimiter = '//',
 		string_after_delimiter = number_string;	
 		
@@ -22,12 +24,31 @@ var StringCalculator = {
 			var index_end_delemiter = number_string.indexOf('\n');
 			delimiter = number_string.substring(index_start_delimiter,index_end_delemiter);
 			string_after_delimiter = number_string.substring(index_end_delemiter+1);
+			
+			var open_regex_str = '\\[';
+			var open_regex = new RegExp(open_regex_str,"gi");
+			delimiter = delimiter.replace(open_regex,'');
+			var close_regex_str = '\\]';
+			var close_regex = new RegExp(close_regex_str,"gi");			
+			delimiter = delimiter.replace(close_regex,',');
+			
+			multi_delimiter = delimiter.split(/,/);
+			if(multi_delimiter.length > 1){
+				var multi_delimiter_string = multi_delimiter.join('|');
+				multi_delimiter_string = multi_delimiter_string.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$]/g, "\\$&");
+				multi_delimiter_string = multi_delimiter_string.substring(0,multi_delimiter_string.length-1);
+
+				delimiter_regex = new RegExp(multi_delimiter_string,"g");
+			}
+			else if(multi_delimiter.length > 0) {
+				delimiter_regex = multi_delimiter[0];				
+			}
 		}
 		else {
 			
 		}
 		
-		total = StringCalculator.CountSumAfterSlit(string_after_delimiter,delimiter);
+		total = StringCalculator.CountSumAfterSlit(string_after_delimiter,delimiter_regex);
 		return total;
 	},
 	CountSumAfterSlit: function (numbers,delimiters){
@@ -40,10 +61,10 @@ var StringCalculator = {
 			if(num) {
 				if(parseInt(num) < 0){
 					nagative_number.push(num);
-				}					
+				}
 				if(parseInt(num) <= 1000){
 					sum += parseInt(num);
-				}
+				}				
 			}		
 		}
 		if(nagative_number.length > 0){
